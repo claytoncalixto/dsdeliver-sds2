66f3d9bcd4419c439f4e950867e07c1455b77549
claytoncalixto <claytonrcalixto@gmail.com>
2021-01-05 00:02:14 -0300

    GET /orders

--- backend/src/main/java/com/claytoncalixto/dsdeliver/repositories/OrderRepository.java
+++ backend/src/main/java/com/claytoncalixto/dsdeliver/repositories/OrderRepository.java
@@ -1,8 +1,15 @@
 package com.claytoncalixto.dsdeliver.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.claytoncalixto.dsdeliver.entities.Order;
 
 public interface  OrderRepository extends JpaRepository<Order, Long> {
+	
+	@Query("SELECT DISTINCT obj FROM Order obj JOIN FETCH obj.products "
+			+ "WHERE obj.status =  0 ORDER BY obj.moment ASC")
+	List<Order> findOrdersWithProducts();
 }
