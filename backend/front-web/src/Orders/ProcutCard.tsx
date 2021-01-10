9f0876baa3742139f2ead6559b10153c30d62a33
claytoncalixto <claytonrcalixto@gmail.com>
2021-01-10 18:18:53 -0300

    Search order location

---
+++ backend/front-web/src/Orders/ProcutCard.tsx
@@ -0,0 +1,39 @@
+
+import { Product } from './types';
+
+type Props = {
+    product: Product;
+}
+
+function formatPrice(price: number){
+    const formatter = new Intl.NumberFormat('pr-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2
+    });
+
+    return formatter.format(price);
+}
+
+function ProcutCard({product}: Props) {
+    return (
+        <div className="orders-card-container">
+            <h3 className="order-card-title">
+                {product.name}
+            </h3>
+            <img 
+               src={product.imageUri}
+               className="order-card-image"   
+               alt={product.name}
+             />
+            <h3 className="orde-card-price">
+              {formatPrice(product.price)}  
+            </h3>      
+            <div className="order-card-description">
+              <h3>Descrição: {product.description}</h3>
+            </div>   
+        </div>
+    )
+}
+
+export default ProcutCard;
--- backend/front-web/src/Orders/index.tsx
+++ backend/front-web/src/Orders/index.tsx
@@ -1,10 +1,27 @@
 import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
 import './styles.css';
+import { useEffect, useState } from 'react';
+import { Product, OrderLocationdata } from './types';
+import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
+
 
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([]);
+    const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
+
+    useEffect(() => {
+      fetchProducts()
+        .then(Response => setProducts(Response.data))
+        .catch( error => console.log(error))
+    }, [])
+
     return (
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products} />
+            <OrderLocation  onChangeLocation={location => setOrderLocation(location)} />
         </div>
     )
 }
